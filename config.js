91bf02eaa442b9e47b364253ba5bfbd1a32dd1b4
bswassim1 <wassimsassi102@gmail.com>
2025-12-08 00:27:12 +0100

    Update project

---
+++ config.js
@@ -0,0 +1,5 @@
+// ⚠️ IMPORTANT: This file is in .gitignore and won't be pushed to GitHub
+// Keep your credentials safe here
+
+const VALID_EMAIL = "wassimamal@gmail.com";
+const VALID_PASSWORD = "amalwassim1208";
